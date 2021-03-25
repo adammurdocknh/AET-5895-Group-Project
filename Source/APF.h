87de28cedc16a66ee3bddaab627bb2253330c5ca
tristanableson <70461660+tristanableson@users.noreply.github.com>
2021-03-25 14:57:00 -0500

    add files

---
+++ Source/APF.h
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    APF.h
+    Created: 25 Mar 2021 2:38:35pm
+    Author:  Tristan Ableson
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class APF{
+
+public:
+ 
+    // Constructor
+    APF();
+    
+    
+    void apf()
+    {
+        
+    }
+    
+    
+private:
+    
+
+    
+};
